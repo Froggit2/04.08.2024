f7999eeee23518aacdb690c53bee49ce747f9f39
Froggit2 <165292357+Froggit2@users.noreply.github.com>
2024-08-05 14:14:13 +0300

    Add files via upload

--- Runner_2.py
+++ Runner_2.py
@@ -23,7 +23,7 @@ class Runner:
 class Tournament:
     def __init__(self, distance, *participants):
         self.full_distance = distance
-        self.participants = list(*participants)
+        self.participants = list(participants)
 
     def start(self):
         finishers = {}
